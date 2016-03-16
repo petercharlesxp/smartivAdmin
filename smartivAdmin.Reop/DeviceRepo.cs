f9415a53453677ef9deee09f15e40cbbf6dd6059
petercharlesxp <petercharles.ca@hotmail.com>
2016-03-16 13:45:13 -0400

    NuGet EF6 in smartivAdmin.Repo for compile error

---
+++ smartivAdmin.Reop/DeviceRepo.cs
@@ -0,0 +1,23 @@
+﻿using smartivAdmin.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smartivAdmin.Reop
+{
+    class DeviceRepo
+    {
+        public class DeviceReop
+        {
+            public List<device> GetDevices()
+            {
+                using (var dbContext = new SmartivContext())
+                {
+                    return dbContext.devices.ToList();
+                }
+            }
+        }
+    }
+}
